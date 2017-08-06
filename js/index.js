fdd520be392c4f0335b9cfbfe8a265e555495028
unknown <soriajose53@yahoo.com>
2017-08-05 20:36:00 -0400

    added maps js

--- js/index.js
+++ js/index.js
@@ -5,3 +5,18 @@ function openRightMenu() {
 function closeRightMenu() {
     document.getElementById("right-menu").style.display = "none";
 }
+
+
+function loadMap() {
+
+	var mapOptions = {
+	   center:new google.maps.LatLng(39.5411164,-82.3972868), zoom:12,
+	   mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
+		
+	var map = new google.maps.Map(document.getElementById("sample"),mapOptions);
+	 var marker = new google.maps.Marker({
+	   position: new google.maps.LatLng(39.5411164,-82.3972868),
+	   map: map,
+	});
+}
