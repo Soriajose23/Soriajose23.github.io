2c5ae5a9ecc2dee196eec3653d7cf018ef3fbc32
Gus Oberdick <augustus_oberdick@mheducation.com>
2017-08-07 16:09:43 -0400

    implemented more refinements. Still have issue with header navigation from about page

--- js/index.js
+++ js/index.js
@@ -73,7 +73,7 @@ function loadMap() {
 	   mapTypeId:google.maps.MapTypeId.ROADMAP
 	};
 		
-	var map = new google.maps.Map(document.getElementById("sample"),mapOptions);
+	var map = new google.maps.Map(document.getElementById("google-map"),mapOptions);
 	 var marker = new google.maps.Marker({
 	   position: new google.maps.LatLng(39.5411164,-82.3972868),
 	   map: map,
